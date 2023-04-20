2c4e46b2042bd04fca494034456d5a27e09bd451
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-04-20 21:24:06 +0000

    oldest tab completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,4 +5,14 @@ def index
   render({ :template => "director_templates/index.html.erb" })
 end
 
+def eldest 
+  @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+  render({ :template => "director_templates/eldest.html.erb" })
+end
+
+def youngest
+
+  render({ :template => "director_templates/youngest.html.erb" })
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
 end
