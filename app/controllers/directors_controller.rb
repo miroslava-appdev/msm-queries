8823431902502f2c03b8254914390cc4cf4ef311
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-04-21 20:28:59 +0000

    bio tab completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -15,4 +15,11 @@ def youngest
   render({ :template => "director_templates/youngest.html.erb" })
 end
 
+def director_details
+  # params looks like this {"an_id"=>"42"}
+  the_id = params.fetch("an_id")
+  @the_director = Director.where({ :id => the_id }).at(0)
+  render({ :template => "director_templates/show.html.erb" })
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 end
